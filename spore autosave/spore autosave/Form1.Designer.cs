0e0eedd38a76d3ea3301ff3312b474cc1c95aeef
love-vinegar <54249550+love-vinegar@users.noreply.github.com>
2020-05-15 21:01:24 +0200

    Add files via upload

---
+++ spore autosave/spore autosave/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace spore_autosave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tick = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.entrovani = new System.Windows.Forms.Timer(this.components);
+            this.nudSavingTime = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PauseResume = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nudSavingTime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tick
+            // 
+            this.tick.Interval = 10000;
+            this.tick.Tick += new System.EventHandler(this.tick_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "end";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // entrovani
+            // 
+            this.entrovani.Tick += new System.EventHandler(this.entrovani_Tick);
+            // 
+            // nudSavingTime
+            // 
+            this.nudSavingTime.Location = new System.Drawing.Point(12, 83);
+            this.nudSavingTime.Name = "nudSavingTime";
+            this.nudSavingTime.Size = new System.Drawing.Size(141, 20);
+            this.nudSavingTime.TabIndex = 2;
+            this.nudSavingTime.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudSavingTime.ValueChanged += new System.EventHandler(this.nudSavingTime_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(7, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "time between saves (in seconds)";
+            // 
+            // PauseResume
+            // 
+            this.PauseResume.Enabled = true;
+            this.PauseResume.Tick += new System.EventHandler(this.PauseResume_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(165, 107);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudSavingTime);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudSavingTime)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tick;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer entrovani;
+        private System.Windows.Forms.NumericUpDown nudSavingTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer PauseResume;
+    }
+}
+
---
+++ spore autosave/spore autosave/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace spore_autosave
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch sw = new Stopwatch();
+        public Form1()
+        {
+            InitializeComponent();
+            this.TopMost = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tick.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tick.Enabled = false;
+        }
+
+        private void tick_Tick(object sender, EventArgs e)
+        {
+            SendKeys.SendWait("^s");
+            sw.Start();
+            entrovani.Enabled = true;
+        }
+
+        private void entrovani_Tick(object sender, EventArgs e)
+        {
+            SendKeys.Send("{ENTER}");
+            if (sw.Elapsed.Seconds > 2)
+            {
+                entrovani.Enabled = false;
+                sw.Stop();
+                sw.Reset();
+            }
+        }
+
+        private void nudSavingTime_ValueChanged(object sender, EventArgs e)
+        {
+            tick.Interval = Convert.ToInt32(nudSavingTime.Value * 1000);
+        }
+        bool Edown = false;
+        private void PauseResume_Tick(object sender, EventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.E))
+            {
+                Edown = true;
+            }
+            if ((Keyboard.IsKeyUp(Key.E) && Edown))
+            {
+                Edown = false;
+                tick.Enabled = !tick.Enabled;
+
+                #region pause/play music
+                if (tick.Enabled)
+                {
+                    Console.Beep(600, 100);
+                    Console.Beep(800, 100);
+                    Console.Beep(900, 100);
+                    Console.Beep(1000, 100);
+                    Console.Beep(1200, 400);
+                }
+                else
+                {
+                    Console.Beep(900, 100);
+                    Console.Beep(800, 100);
+                    Console.Beep(700, 100);
+                    Console.Beep(600, 100);
+                    Console.Beep(400, 400);
+                }
+                #endregion
+            }
+        }
+    }
+}
